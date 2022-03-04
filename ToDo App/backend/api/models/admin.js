20b0b510cb1c2e43a053d983d0d191842ddb9349
ChennuriPreethi <chennuripreethi25@gmail.com>
2022-03-04 18:50:16 +0530

    Updated OAuth

---
+++ ToDo App/backend/api/models/admin.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/Task1', {useNewUrlParser: true});
+
+mongoose.connect('mongodb+srv://todoapp:todo123@cluster0.zlk0r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+var conn =mongoose.Collection;
+ 
+var adminSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  _UserID:{
+    type: mongoose.Types.ObjectId,
+    required:true
+  },
+  completed: {
+    type: Boolean
+    // default: false
+  }
+});
+ 
+var adminModel = mongoose.model('Admins', adminSchema);
+module.exports=adminModel;
